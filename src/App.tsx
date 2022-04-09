b14b3cd49a480c1f7950bfa6ffed387d46432019
Andrew Burgess <andrew@burgess.io>
2022-04-09 16:02:03 -0400

    can choose lines to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { useState, useEffect } from "react";
-import highlight from "highlight.js";
-import ts from "highlight.js/lib/languages/typescript";
+import { useState, useEffect, useCallback } from "react";
+// import highlight from "highlight.js";
+// import ts from "highlight.js/lib/languages/typescript";
 import "highlight.js/styles/nord.css";
 import "./App.css";
 
-highlight.registerLanguage("typescript", ts);
+// highlight.registerLanguage("typescript", ts);
 
-const code1 = `
+const textColors = ["#81a1c1", "#d8dee9", "#8fbcbb"] as const;
+
+type CodeChar = {
+  char: string;
+  color: typeof textColors[number];
+};
+
+function toChars(code: string): CodeChar[][] {
+  return code
+    .trim()
+    .split("\n")
+    .map((line) =>
+      line.split("").map((char) => ({
+        char: char,
+        color: textColors[0],
+        // color: textColors[i++ % textColors.length]
+      }))
+    );
+}
+
+const code1 = toChars(`
 type AnyFunction = (...args: any[]) => any;
 
 function optional<Fn extends AnyFunction>(fn: Fn, errHandler?: (e: any) => void) {
@@ -18,9 +38,9 @@ function optional<Fn extends AnyFunction>(fn: Fn, errHandler?: (e: any) => void)
 
 
 }
-`.trim();
+`);
 
-const code2 = `
+const code2 = toChars(`
 type AnyFunction = (...args: any[]) => any;
 
 function optional<Fn extends AnyFunction>(fn: Fn, errHandler?: (e: any) => void) {
@@ -32,9 +52,9 @@ function optional<Fn extends AnyFunction>(fn: Fn, errHandler?: (e: any) => void)
 
   }
 }
-`.trim();
+`);
 
-const code3 = `
+const code3 = toChars(`
 type AnyFunction = (...args: any[]) => any;
 
 function optional<Fn extends AnyFunction>(fn: Fn, errHandler?: (e: any) => void) {
@@ -46,47 +66,83 @@ function optional<Fn extends AnyFunction>(fn: Fn, errHandler?: (e: any) => void)
       }
   }
 }
-`.trim();
+`);
 
-const slides = [code1, code2, code3];
+function slice2D<T>(arr: T[][], idx: number): T[][] {
+  const ret: T[][] = [];
+
+  for (const item of arr) {
+    if (item.length >= idx) {
+      ret.push(item.slice(0, idx));
+      return ret;
+    } else {
+      idx -= item.length;
+      ret.push(item);
+    }
+  }
+  return ret;
+}
+
+function clearLines(arr: CodeChar[][], lineNos: number[]): CodeChar[][] {
+  return arr.map((line: CodeChar[], idx: number) =>
+    lineNos.includes(idx)
+      ? line
+      : line.map((char: CodeChar) => ({ ...char, char: " " }))
+  );
+}
+
+const stages = [
+  [0, 1, 2, 10],
+  [0, 1, 2, 3, 9, 10],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+];
 
 function App() {
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [charCount, setCharCount] = useState(10);
-  const out = highlight
-    .highlight(code1.trim().slice(0, charCount), {
-      language: "typescript",
-    })
-    .value.split("\n");
-
-  useEffect(() => {
-    setTimeout();
-
-    // const id = setInterval(() => setCharCount((c) => c + 1), 20);
-    // return () => clearInterval(id);
-  }, []);
-
-  return (
-    <div className="hljs" style={wrapperStyle}>
-      {code1
-        .trim()
-        .split("\n")
-        .map((_, idx) => (
-          <div style={rowStyle}>
-            <div style={{ ...codeStyle, ...lineNumStyle }}>{idx + 1}</div>
-            <pre
-              style={codeStyle}
-              dangerouslySetInnerHTML={{ __html: out[idx] }}
-            />
-          </div>
-        ))}
-    </div>
-  );
+  return <CodeContainer code={code3} />;
 }
 
 export default App;
 
-const Line = ({ lineNo, code }: { lineNo: number; code: string }) => {};
+const CodeContainer = ({ code }: { code: CodeChar[][] }) => {
+  const totalLength = code.reduce((acc, next) => acc + next.length, 0);
+  const [len, setLen] = useState(0);
+
+  const handler = useCallback(
+    (evt: { key: string }) => {
+      if (evt.key === "ArrowRight" && len <= totalLength) {
+        setLen((i) => i + 1);
+      }
+      if (evt.key === "ArrowLeft" && len <= totalLength) {
+        setLen((i) => i - 1);
+      }
+    },
+    [len, totalLength]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handler);
+    return () => document.removeEventListener("keydown", handler);
+  }, [handler]);
+
+  return <Code code={clearLines(code, stages[len])} />;
+};
+
+const Code = ({ code }: { code: CodeChar[][] }) => {
+  return (
+    <div className="hljs" style={wrapperStyle}>
+      {code.map((line, idx) => (
+        <div key={idx} style={rowStyle}>
+          <div style={{ ...codeStyle, ...lineNumStyle }}>{idx + 1}</div>
+          {line.map((char, charIdx) => (
+            <pre key={charIdx} style={{ ...codeStyle, color: char.color }}>
+              {char.char}
+            </pre>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const wrapperStyle = {
   height: "100%",
@@ -114,7 +170,3 @@ const codeStyle = {
   fontWeight: "bold",
   margin: 0,
 };
-
-function explodeMarkup(code: string): string {
-  return code;
-}
